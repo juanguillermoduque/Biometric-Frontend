e75106e5cc8926e954954ec1c6b311146241f78f
andresgomezquiroga <andresgomezquiroga123@gmail.com>
2023-03-24 18:14:13 -0500

    login angula material

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AuthComponent } from './auth/auth.component';
-import { AdministradorComponent } from './administrador/administrador.component';
 
 const routes: Routes = [
-  { path: '', component: AuthComponent },
-  { path: 'Administrador', component: AdministradorComponent }
+
 ];
 
 @NgModule({
--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-auth',
@@ -7,23 +8,36 @@ import { Router } from '@angular/router';
   styleUrls: ['./auth.component.css']
 })
 export class AuthComponent {
-
   rol: string = '';
   documento: string = '';
   contrasena: string = '';
-  mostrarError = false;
+  mostrarError: boolean = false;
 
-  constructor(private router: Router) {}
+
+
+  constructor(
+    private router: Router,
+    private _snackBar: MatSnackBar
+  ) {}
+
+  navigateTo(page: string) {
+    this.router.navigateByUrl('/administrador' + page);
+  }
 
   onSubmit() {
     if (!this.rol || !this.documento || !this.contrasena) {
-      this.mostrarError = true;
+      this._snackBar.open('Por favor completa todos los campos.', 'Cerrar', {
+        duration: 5000,
+        horizontalPosition: 'center',
+        verticalPosition: 'top'
+      });
     } else {
+      this.mostrarError = false;
       this.rol = '';
       this.documento = '';
       this.contrasena = '';
-      this.mostrarError = false;
-      this.router.navigate(['/administrador/fichas']);
     }
   }
 }
+
+
