f389d3d3f033cb33aa0f0d92facf72254e038c6c
Kevin Loaiza <117102238+KevinLC02@users.noreply.github.com>
2023-03-28 19:17:16 -0500

    Creacion de TS de asistencias backend

--- src/app/asistencias/editar-asistencias/editar-asistencias.component.ts
+++ src/app/asistencias/editar-asistencias/editar-asistencias.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { asistencia } from 'src/app/models/asistencia';
+import { AsistenciasService } from 'src/app/services/asistencias/asistencias.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-editar-asistencias',
@@ -6,5 +9,54 @@ import { Component } from '@angular/core';
   styleUrls: ['./editar-asistencias.component.css']
 })
 export class EditarAsistenciasComponent {
+
+  asistencia: asistencia ={
+    idasistencia:0,
+    date_enter:'',
+    date_start:'',
+    date_end:'',
+    aula:0,
+    Estado:'',
+    comments:'',
+    created_at :'',
+    updated_at :'',
+  };
+params = this.activeRouted.snapshot.params;
+
+  constructor(private asistenciasService:AsistenciasService, private activeRouted:ActivatedRoute){
+
+  }
+
+
+  ngOnInit(){
+    if(this.params['id']){
+      this.asistenciasService.getAsistencia(this.params['id'])
+        .subscribe(
+          res=>{
+            console.log(res);
+      
+          },
+          err => console.error(err)
+        )
+
+  }
+}
+  
+  modificarAsistencia(){
+    delete this.asistencia.created_at;
+    delete this.asistencia.updated_at;
+    delete this.asistencia.date_enter;
+    delete this.asistencia.date_start;
+    delete this.asistencia.date_end;
+
+  this.asistenciasService.updateAsistencia(this.params['id'],this.asistencia)
+      .subscribe(
+        res =>{
+          console.log(res);
+        },
+        err => console.error(err)
+      )
+    }
+
 
 }
