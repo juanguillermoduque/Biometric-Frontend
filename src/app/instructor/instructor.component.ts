e45f2f18497440962a864c4d63dcac05d8fade87
andresgomezquiroga <andresgomezquiroga123@gmail.com>
2023-03-29 21:30:27 -0500

    typescript instructor

--- src/app/instructor/instructor.component.ts
+++ src/app/instructor/instructor.component.ts
@@ -9,14 +9,20 @@ import { Router } from '@angular/router';
 export class InstructorComponent {
 
   activaOpcion: number = 0;
+  fichaSeleccionada: number = 0;
+
+  constructor(private router: Router) {}
 
   AccionInstructor(opcion: number) {
     this.activaOpcion = opcion;
   }
+
+  editarFicha(numeroFicha: number) {
+    this.fichaSeleccionada = numeroFicha;
+    this.router.navigate(['/editar-ficha', this.fichaSeleccionada]);
+  }
+
 }
-
-
-
 
 
 
