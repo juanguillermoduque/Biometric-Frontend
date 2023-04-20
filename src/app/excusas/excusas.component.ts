fa6a1a8ea24d1e9243676a1a11b713e5a0460ccc
Jhojan David Agudelo <jdagudelo540@misena.edu.co>
2023-04-19 22:08:43 -0500

    no completo

--- src/app/excusas/excusas.component.ts
+++ src/app/excusas/excusas.component.ts
@@ -1,16 +1,54 @@
 import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ExcusasService } from '../services/excusas/excusas.service';
+import {MatDialog, MatDialogRef} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-excusas',
   templateUrl: './excusas.component.html',
   styleUrls: ['./excusas.component.css']
 })
-export class ExcusasComponent {
+
+export class ExcusasComponent implements OnInit{
   displayedColumns: string[] = ['N°', 'Fecha', 'Instructor/a', 'Estado'];
   excusas:any = ['','','','','',''];
   dataSource = this.excusas;
+
+  constructor(
+    private excusaService: ExcusasService,
+    public dialog: MatDialog,
+    ){
+
+  }
+  ngOnInit(){
+    this.fichaService.getFichas().subscribe(
+      res =>{
+        this.fichas = res;
+        console.log(this.fichas);
+      },
+      err=>console.error(err)
+    )
   }
 
+  agregarFicha(){
+    this.dialog.open(AgregarFichasComponent, {
+      height: '500px',
+      width: '600px',
+    });
+  }
+
+  editarFicha(idFicha :number){
+
+    this.dialog.open(EditarFichasComponent, {
+      height: '800px',
+      width: '600px',
+      data: idFicha,
+    });
+  }
+
+}
+
+  
   export interface PeriodicElement {
     numero: number;
     fecha: string;
