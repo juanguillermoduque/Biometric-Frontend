1484192ad8e6e9554da5928ccbd618041e477884
Kevin Loaiza <117102238+KevinLC02@users.noreply.github.com>
2023-04-20 17:22:09 -0500

    creacion de services de horarios y modificaciones

--- src/app/horarios/horarios.component.ts
+++ src/app/horarios/horarios.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HorariosService } from '../services/horarios/horarios.service';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-horarios',
   templateUrl: './horarios.component.html',
   styleUrls: ['./horarios.component.css']
 })
-export class HorariosComponent {
+export class HorariosComponent implements OnInit{
+editarhorarios(arg0: any) {
+throw new Error('Method not implemented.');
+}
+agregarHorario() {
+throw new Error('Method not implemented.');
+}
+  
+  horarios:any = [];
+  dataSource = this.horarios;
+
+  constructor(
+    private horarioService: HorariosService,
+    public dialog: MatDialog,
+    ){
+
+}
+
+ngOnInit(){
+  this.horarioService.getHorarios().subscribe(
+    res =>{
+      this.horarios = res;
+      console.log(this.horarios);
+    },
+    err=>console.error(err)
+  )
+}
 
 }
--- src/app/services/horarios/horarios.service.ts
+++ src/app/services/horarios/horarios.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { horario } from 'src/app/models/horarios';
 @Injectable({
   providedIn: 'root'
 })
 export class HorariosService {
+  API_URI = 'http://localhost:3000/api';
 
-  constructor() { }
+  constructor(private http:HttpClient) {
+
+  }
+  getHorarios(){
+    return this.http.get(`${this.API_URI}/horarios/`);
+  }
+  getHorario(id:number){
+    return this.http.get(`${this.API_URI}/horarios/${id}`);
+  }
+  saveHorario(horario:horario){
+    return this.http.post(`${this.API_URI}/horarios/`,horario);
+  }
+  updateFicha(id:number,horario:horario):Observable<horario>{
+    return this.http.put(`${this.API_URI}/horario/editar${id}`,horario);
+  }
 }
