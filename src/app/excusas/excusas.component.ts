aeec3f91468510d9f98c5fef990241608a1229cf
Jhojan David Agudelo <jdagudelo540@misena.edu.co>
2023-04-25 03:14:06 -0500

    comentarios usuarios-excusas

--- src/app/excusas/excusas.component.ts
+++ src/app/excusas/excusas.component.ts
@@ -1,49 +1,51 @@
 
-import { Component, OnInit } from '@angular/core';
-import { ExcusasService } from '../services/excusas/excusas.service';
-import {MatDialog, MatDialogRef} from '@angular/material/dialog';
-import { CrearExcusaComponent } from './crear-excusa/crear-excusa.component';
-import { EditarExcusasComponent } from './editar-excusas/editar-excusas.component';
+import { Component, OnInit } from '@angular/core';  /*importación del componente OnInit*/
+import { ExcusasService } from '../services/excusas/excusas.service'; /* importación del servicio ExcusasService que hace una conexión con el backend*/
+import {MatDialog, MatDialogRef} from '@angular/material/dialog'; // importación del componente MatDialog
+import { CrearExcusaComponent } from './crear-excusa/crear-excusa.component'; // importación del componente CrearExcusas
+import { EditarExcusasComponent } from './editar-excusas/editar-excusas.component'; // importación del componente EditarExcusa
 
-@Component({
-  selector: 'app-excusas',
-  templateUrl: './excusas.component.html',
-  styleUrls: ['./excusas.component.css']
+@Component({ /* es un decorador que se utiliza para configurar las propiedades del componente "excusas*/
+  selector: 'app-excusas', /* es una cadena de texto que se utiliza para identificar y usar el componente en las plantillas HTML de la aplicación */
+  templateUrl: './excusas.component.html',  // es una cadena de texto que especifica la ruta del archivo de plantilla HTML asociado con el componente
+  styleUrls: ['./excusas.component.css'] // especifica las rutas de los archivos de hojas de estilo CSS asociados con el componente
 })
 
-export class ExcusasComponent implements OnInit{
-  displayedColumns: string[] = ['N°', 'Fecha', 'Instructor/a', 'Estado'];
-  excusas:any = ['','','','','',''];
-  dataSource = this.excusas;
+export class ExcusasComponent implements OnInit{ // llamado de componente Excusas implementando la interfaz OnInit
+  displayedColumns: string[] = ['N°', 'Fecha', 'Instructor/a', 'Estado']; // Arreglo de columnas para mostrar en una tabla 
+  excusas:any = ['','','','','','']; // variable excusa que es un arreglo de 6 valores vacíos todos de tipo string
+  dataSource = this.excusas; // se utiliza como fuente de datos para la tabla
 
   constructor(
-    private excusaService: ExcusasService,
-    public dialog: MatDialog,
+    private excusaService: ExcusasService, // definición de ExcusasService que tiene la conexión con el back
+    public dialog: MatDialog, // MatDialog proporciona una ventana emergente en la cual se puede ingresar información sin la necesidad de cambiar de ruta
     ){
 
   }
-  ngOnInit(){
-    this.excusaService.getexcusas().subscribe(
-      res =>{
+  ngOnInit(){ // el ngOnInit se ejecuta cuando se inicializa el componente
+    // se realiza el llamado al servicio excusaService para obtener datos de las excusas 
+    this.excusaService.getexcusas().subscribe( // utilizado para subscribirse a un flujo de eventos y recibir notificaciones de cuando ocurra un cambio
+      res =>{ // si la respuesta por parte del servidor es exitosa se asigna el valor de res a this.excusas y luego se muestra por pantalla el valor de excusas
         this.excusas = res;
         console.log(this.excusas);
       },
-      err=>console.error(err)
+      err=>console.error(err) // de lo contrario saldrá un error
     )
   }
 
-  crearExcusa(){
+  crearExcusa(){ // Método crearExcusa que me muestra una ventana emergente con el componente CrearExcusa
     this.dialog.open(CrearExcusaComponent, {
       height: '500px',
       width: '600px',
     });
   }
 
-  editarExcusa(idExcusa :number){
+  editarExcusa(idExcusa :number){ // Método editarExcusa que me muestra una ventana emergente con el componente EditarExcusa
+    // como parámetro me recibira el valor de idExcusa
     this.dialog.open(EditarExcusasComponent, {
       height: '800px',
       width: '600px',
-      data: idExcusa,
+      data: idExcusa, // data almacenará el valor de este
     });
   }
 
