62af8ecb6ae173db416b1698daf7d3fc2b09ef75
Juan Guillermo <g.duque@utp.edu.co>
2023-06-26 19:55:13 -0500

    cambios

--- src/app/horarios/componentes/editar-horarios/editar-horarios.component.ts
+++ src/app/horarios/componentes/editar-horarios/editar-horarios.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { horario } from '../../horarios';
 import { HorariosService } from '../../horarios.service';
 import Swal from 'sweetalert2'
 import { UsuariosService } from 'src/app/usuarios/usuarios.service';
 import { RolesService } from 'src/app/roles/roles.service';
 import { FichasService } from 'src/app/fichas/fichas.service';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-editar-horarios',
@@ -35,7 +36,73 @@ export class EditarHorariosComponent{
   };
   
   constructor(private horarioService:HorariosService, private rolesService:RolesService, 
-  private usuariosService:UsuariosService, private fichasService:FichasService){}
+  private usuariosService:UsuariosService, private fichasService:FichasService, 
+  public dialogRef: MatDialogRef<EditarHorariosComponent>,
+  @Inject(MAT_DIALOG_DATA) public data:number
+  ){}
+
+  normalizarDate(date:Date|null){
+  if(date == null){
+    return '';
+  }
+    let day = date.getDate()
+    let month = date.getMonth() + 1
+    let year = date.getFullYear()
+    if(month < 10){
+      return(`${year}-0${month}-${day}`)
+    }else{
+      return(`${year}-${month}-${day}`)
+    }
+  }
+
+  ngOnInit(): void {
+    console.log(this.data)
+
+  }
+
+  getHorario(){
+    
+    this.horarioService.getHorario(this.data).subscribe(
+      (res)=>{
+        console.log(res);
+        this.horario = res;
+      }
+    )
+  }
+  
+  getIdUsuario(){
+    this.rolesService.searchInstructores().subscribe(
+      (instructores) => {
+        this.instructoresIdRol = instructores;
+        this.getUsuario();
+      }
+    );
+  }
+  getUsuario(){
+    for(let i = 0; i < this.instructoresIdRol.length; i++){
+      this.instructoresId.push(this.instructoresIdRol[i].id_usuario)
+    }
+    this.getInstructor();
+  }
+  getInstructor(){
+    for(let i = 0; i < this.instructoresId.length; i++){
+      this.usuariosService.getUsuario(this.instructoresId[i]).subscribe(
+        (data)=>{
+          this.instructores.push(data);
+        }
+      )
+    }
+  }
+
+  getFichas(){
+    this.fichasService.getFichas().subscribe(
+      (fichas) => {
+        this.fichasAux = fichas;
+        this.fichas = this.fichasAux[0];
+      }
+    );
+  }
+
 
   agregarInstructor(instructor:any){
     this.fichaInstructor.id_instructor = instructor.num_id;
@@ -70,9 +137,13 @@ export class EditarHorariosComponent{
             position: 'center',
             icon: 'success',
             title: 'El horario fue modificado exitosamente',
-            showConfirmButton: false,
-            timer: 1500
-          })
+            showConfirmButton: true,
+            
+          }).then((result) => {
+            if (result.isConfirmed) {
+              this.dialogRef.close();
+            } 
+          });
           console.log(res);
         },
         err => console.error(err)
