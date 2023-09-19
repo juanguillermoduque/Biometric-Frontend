9da9b50580c994cf73d89ffc2ee88ede9c522bf9
Jhojan David Agudelo Benjumea <jdagudelo540@misena.edu.co>
2023-09-19 11:57:02 -0500

    nuevo

--- src/app/globals.ts
+++ src/app/globals.ts
@@ -1,4 +1,3 @@
-//export const API_DOMAIN:string = 'http://localhost:3000/api'; 
+export const API_DOMAIN:string = 'http://localhost:3000/api'; 
 
-export const API_DOMAIN:string = 'https://biometricsena.us/api';
-; 
+//export const API_DOMAIN:string = 'https://biometricsena.us/api';
